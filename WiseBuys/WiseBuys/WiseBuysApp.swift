a5ee55cfe00d765506201fd7d2bda5e468802acf
Lauren Ferlito <lferlito@unc.edu>
2023-02-11 16:35:30 -0500

    creating Xcode file

---
+++ WiseBuys/WiseBuys/WiseBuysApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseBuysApp.swift
+//  WiseBuys
+//
+//  Created by Lauren Ferlito on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WiseBuysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
