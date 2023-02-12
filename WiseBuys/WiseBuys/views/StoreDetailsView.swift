c1f5487efe03481ba7485af669e619c1f3bd1f39
Lauren Ferlito <lferlito@unc.edu>
2023-02-12 03:11:13 -0500

    google maps

--- WiseBuys/WiseBuys/views/StoreDetailsView.swift
+++ WiseBuys/WiseBuys/views/StoreDetailsView.swift
@@ -108,17 +108,29 @@ struct StoreDetailsView: View {
                             
                         }
                     })
-                    NavigationLink(destination: ListView(), label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 20)
-                                .frame(height: 90)
-                                .foregroundColor(Color("blueAccent"))
-                                .padding(.trailing, 20)
-                            Text("Directions")
-                                .font(.custom(font, size: 30))
-                                .foregroundColor(.white)
-                        }
-                    })
+//                    NavigationLink(destination: ListView(), label: {
+//                        ZStack{
+//                            RoundedRectangle(cornerRadius: 20)
+//                                .frame(height: 90)
+//                                .foregroundColor(Color("blueAccent"))
+//                                .padding(.trailing, 20)
+//                            Text("Directions")
+//                                .font(.custom(font, size: 30))
+//                                .foregroundColor(.white)
+//                        }
+//                    })
+                    Button(action: {
+                                UIApplication.shared.open(URL(string: "https://www.google.com/maps/")!)
+                            }) {
+                                Text("Directions")
+                                    .font(.custom(font, size: 30))
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: 165, height: 90)
+                            .background(Color("blueAccent"))
+                                   .foregroundColor(.white)
+                                   .cornerRadius(20)
+                                   .padding(.trailing, 20)
                     }
                 }
             }
