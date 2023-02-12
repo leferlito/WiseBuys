f29c59bd387721da8226b26fe6c2c08d058504aa
Mackenzie Perry <mperry2@unc.edu>
2023-02-11 23:43:33 -0500

    added kieran's HomeView

---
+++ WiseBuys/WiseBuys/views/HomeView.swift
@@ -0,0 +1,57 @@
+//
+//  HomeView.swift
+//  WiseBuys
+//
+//  Created by Mackenzie Perry on 2/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    let font: String = "Yellowtail-Regular"
+        let font2: String = "YesevaOne-Regular"
+        var body: some View {
+            NavigationStack{
+                ZStack(){
+                    Color("blueBackground")
+                        .ignoresSafeArea()
+                    VStack(spacing: 5){
+                        
+                        Text("Wise Buys")
+                            .font(.custom(font2, size: 65))
+                            .foregroundColor(Color("darkGreen"))
+                            .padding(.top, 40)
+                        Text("Happy shopping!")
+                            .font(.custom(font, size: 40))
+                            .foregroundColor(Color("darkGreen"))
+                        Image("WiseBuysLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 450, height: 380)
+                            
+                            NavigationLink(destination: ListView(), label: {
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .frame(width: 345, height: 90, alignment: .center)
+                                        .foregroundColor(Color("midBlue"))
+                                    Text("Current Trip")
+                                        .font(.custom(font2, size: 30))
+                                        .foregroundColor(Color("darkGreen"))
+                                }})
+                            
+                        
+                        Spacer()
+                        
+                        
+                    }
+                }
+                
+            }
+        }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ WiseBuys/WiseBuys/views/List/ListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ListViewModel.swift
+//  WiseBuys
+//
+//  Created by Lauren Ferlito on 2/11/23.
+//
+
+import Foundation
+
+class ListViewModel: ObservableObject {
+    
+    @Published var listItems: [ListItem] = [
+        ListItem(name: "item 1", price: "2.00", store: "Aldi"),
+        ListItem(name: "item 2", price: "2.00", store: "Aldi"),
+        ListItem(name: "item 3", price: "2.00", store: "Aldi")
+    ]
+    
+    
+    @Published var selected_products: [String] = ["test"]
+    
+    
+    func addItem(item: String){
+        self.selected_products.append(item)
+    }
+    
+}
