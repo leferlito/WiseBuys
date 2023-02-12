16557c7c53150086651bf66422915505196ad1b9
Kiernan Almand <97690836+kiernan-a@users.noreply.github.com>
2023-02-11 23:08:45 -0500

    added accent colors and fonts

--- WiseBuys/WiseBuys/HomeView.swift
+++ WiseBuys/WiseBuys/HomeView.swift
@@ -9,36 +9,44 @@ import SwiftUI
 
 struct Home: View {
     let font: String = "Yellowtail-Regular"
+    let font2: String = "YesevaOne-Regular"
     var body: some View {
-        ZStack(){
-            Color("blueBackground")
-                .ignoresSafeArea()
-            VStack(spacing: 5){
-               
-                Text("Wise Buys")
-                    .font(.custom(font, size: 80))
-                    .foregroundColor(Color("darkGreen"))
-                    .padding(.top, 40)
-                Text("Happy shopping!")
-                    .font(.custom(font, size: 40))
-                    .foregroundColor(Color("darkGreen"))
-                Image("WiseBuysLogo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 450, height: 380)
-                ZStack(){
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 345, height: 90, alignment: .center)
-                        .foregroundColor(Color("blueAccent"))
+        NavigationStack{
+            ZStack(){
+                Color("blueBackground")
+                    .ignoresSafeArea()
+                VStack(spacing: 5){
+                    
+                    Text("Wise Buys")
+                        .font(.custom(font2, size: 65))
+                        .foregroundColor(Color("darkGreen"))
+                        .padding(.top, 40)
+                    Text("Happy shopping!")
+                        .font(.custom(font, size: 40))
+                        .foregroundColor(Color("darkGreen"))
+                    Image("WiseBuysLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 450, height: 380)
+                        
+                        NavigationLink(destination: ListView(), label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 20)
+                                    .frame(width: 345, height: 90, alignment: .center)
+                                    .foregroundColor(Color("midBlue"))
+                                Text("Current Trip")
+                                    .font(.custom(font2, size: 30))
+                                    .foregroundColor(Color("darkGreen"))
+                            }})
+                        
+                    
+                    Spacer()
+                    
                     
                 }
-                Spacer()
-                
-                
             }
+            
         }
-        
-        
     }
 }
 
