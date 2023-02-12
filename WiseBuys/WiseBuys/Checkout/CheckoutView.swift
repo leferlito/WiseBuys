570ebb724403794115ef3c6c9d39332cd80a088f
Ciya Joseph <ciyajoseph05@gmail.com>
2023-02-12 03:17:14 -0500

    checkout figma

--- WiseBuys/WiseBuys/Checkout/CheckoutView.swift
+++ WiseBuys/WiseBuys/Checkout/CheckoutView.swift
@@ -15,36 +15,76 @@ struct CheckoutView: View {
 
     var body: some View {
         VStack{
-            ForEach(0..<4, id: \.self){ index in
-                HStack{
-                    if vm.stores.count > 0{
-                        if vm.stores[index] == .Aldi {
-                            Text("Aldi")
-                        }
-                        else if vm.stores[index] == .Kroger {
-                            Text("Kroger")
-                            
-                        }
-                        else if vm.stores[index] == .TraderJoes {
-                            Text("Trader Joes")
-                            
-                        }
-                        else if vm.stores[index] == .Publix {
-                            Text("Publix")
-                            
-                        }
-                        else if vm.stores[index] == .Walmart {
-                            Text("Walmart")
-                            
-                        }
-                    }
-                    Text("\(vm.sorted_costs[index])")
-                }
+            Spacer()
+//                .frame(height: 43)
+                Text("Checkout")
+                .font(.system(size: 60).weight(.bold))
+                    .foregroundColor(.darkGreen)
+            
+            Spacer()
+//                .frame(height: 65)
+            HStack {
+                Text("Edit")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .frame(width: 86, height: 35)
+                    .background(Color.darkGreen)
+                    .cornerRadius(20)
+                Spacer()
             }
-        }.onAppear{
-            vm.find_best(products: products)
-        }
+            .padding(.horizontal, 39)
+            Spacer()
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .fill(Color("lightGreen"))
+                    .frame(height: 335)
+                
+                ForEach(0..<4, id: \.self){ index in
+                    ZStack (alignment: .leading){
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .fill(Color.darkGreen)
+                        HStack{
+                            if vm.stores.count > 0{
+                                if vm.stores[index] == .Aldi {
+                                    Text("Aldi")
+                                }
+                                else if vm.stores[index] == .Kroger {
+                                    Text("Kroger")
+                                    
+                                }
+                                else if vm.stores[index] == .TraderJoes {
+                                    Text("Trader Joes")
+                                    
+                                }
+                                else if vm.stores[index] == .Publix {
+                                    Text("Publix")
+                                    
+                                }
+                                else if vm.stores[index] == .Walmart {
+                                    Text("Walmart")
+                                    
+                                }
+                            }
+                        
+                            Spacer()
+                            Text("\(vm.sorted_costs[index])")
+                        }.padding(.horizontal)
+                        
+                }.padding(.horizontal)
+            }
+            Spacer()
+            }
+        .background(Color.blueBackground)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear{
+                vm.find_best(products: products)
+            }
     }
+}
+
+extension Color {
+    static let lightGreen = Color("lightGreen")
+    static let darkGreen = Color("darkGreen")
+    static let blueBackground = Color("blueBackground")
 }
 
 struct CheckoutView_Previews: PreviewProvider {
