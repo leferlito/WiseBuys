00d9c6efec2e3d899c45a92aaabd420fa9d19023
Mackenzie Perry <mperry2@unc.edu>
2023-02-12 01:40:30 -0500

    list view update looks

--- WiseBuys/WiseBuys/views/List/ListView.swift
+++ WiseBuys/WiseBuys/views/List/ListView.swift
@@ -12,30 +12,105 @@ struct ListView: View {
     @StateObject private var vm = ListViewModel()
     @State var bring_up_search_bar = false
     
-    @State var item = "hen"
+    @State var item = ""
     
     var body: some View {
-        VStack{
-            Text("\(item)")
-            List{
-                ForEach(vm.selected_products, id: \.self){ item in
-                    Text(item)
+        NavigationView {
+            ZStack{
+                Color("blueBackground")
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Grocery List")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.bottom, 50)
+                        .foregroundColor(Color("blueAccent"))
+                    
+                    
+                    // Text("\(item)")
+                    Button {
+                        bring_up_search_bar = true
+                    } label: {
+                        HStack{
+//                            Image(systemName: "plus")
+//                                .padding()
+//                                .foregroundColor(Color(.black))
+                              
+                     
+
+                            HStack {
+//                                Spacer()
+                                HStack {
+                                    Spacer()
+                                    Text("Add Item")
+                                        .foregroundColor(Color(.black))
+                                        .padding()
+                                    Spacer()
+                                }
+//                                Spacer()
+                                
+                            }
+            
+                                
+                        }
+                        .frame(width: 325, height: 45, alignment: .center)
+                        .background(Color("midBlue"))
+                        .cornerRadius(12)
+                    }
+                   // .padding(.vertical, 5)
+                    
+                    //            Button("Add Item"){
+                    //                bring_up_search_bar = true
+                    //            }
+                    VStack{
+                        Spacer()
+                        ScrollView {
+                            VStack{
+                                ForEach(vm.selected_products, id: \.self){ item in
+                                   
+                                    if (vm.selected_products[0].isEmpty) {
+                                        Image(systemName: "cart.fill")
+                                            .foregroundColor(Color("blueAccent"))
+                                    }
+                                    HStack {
+                                        Text(item)
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    // .padding(.vertical)
+                                        .foregroundColor(Color(.white))
+                                        .frame(width: 300, height: 35, alignment: .center)
+                                        .background(Color("blueAccent"))
+                                        .cornerRadius(12)
+                                }
+                            }
+                            //padding(.vertical)
+                        }
+                    }
+                    .frame(width: 325, height: 400, alignment: .center)
+                    .background(Color("midBlue"))
+                    .cornerRadius(12)
+                    .padding(.vertical,20)
+                    
+                    Button {
+                        print("checkout")
+                    } label: {
+                        Text("Checkout")
+                            .foregroundColor(Color(.black))
+                            .frame(width: 325, height: 45, alignment: .center)
+                            .background(Color("midBlue"))
+                            .cornerRadius(12)
+                    }
+                }
+                .sheet(isPresented: $bring_up_search_bar){
+                    SearchView(item: $item, stayOpen: $bring_up_search_bar)
+                }
+                .onChange(of: item){ item_name in
+                    print("add item")
+                    vm.addItem(item: item_name)
                 }
             }
-            
-            Button("Add Item"){
-                bring_up_search_bar = true
-            }
-            Button("Checkout"){
-                print("checkout")
-            }
-        }
-        .sheet(isPresented: $bring_up_search_bar){
-            SearchView(item: $item, stayOpen: $bring_up_search_bar)
-        }
-        .onChange(of: item){ item_name in
-            print("add item")
-            vm.addItem(item: item_name)
+            //  .background("blueBackground")
         }
     }
 }
