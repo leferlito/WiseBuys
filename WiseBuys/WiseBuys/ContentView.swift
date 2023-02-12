6c1aeccf40617d42afcb70aa71b8a4657e9d5997
Ciya Joseph <ciyajoseph05@gmail.com>
2023-02-12 01:16:27 -0500

    checkout

--- WiseBuys/WiseBuys/ContentView.swift
+++ WiseBuys/WiseBuys/ContentView.swift
@@ -10,24 +10,24 @@ import SwiftUI
 struct ContentView: View {
     let font: String = "Yellowtail-Regular"
     var body: some View {
-//        NavigationStack{
-//            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundColor(.accentColor)
-//                Text("Hello, world!")
-//                    .font(.custom(font, size: 30))
-//            }
-//            .padding()
-//            .navigationTitle("Wise Buys")
-//
-//        }
-        CheckoutView(products: .constant([
-            "Carrots, whole",
-            "Cheese, shredded, Mexican style",
-            "Tea, green, bags",
-            "Sugar, brown"
-        ]))
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                    .font(.custom(font, size: 30))
+            }
+            .padding()
+            .navigationTitle("Wise Buys")
+
+        }
+//        CheckoutView(products: .constant([
+//            "Carrots, whole",
+//            "Cheese, shredded, Mexican style",
+//            "Tea, green, bags",
+//            "Sugar, brown"
+//        ]))
         
     }
 }
